3d7aa10b20f0c32f9ddbf08b98ee202e22777c05
tudicodings <tudorapavaloaie@gmail.com>
2026-06-14 22:34:43 +0300

    versiune licenta

--- app/src/main/java/com/example/inventarasit/MenuActivity.kt
+++ app/src/main/java/com/example/inventarasit/MenuActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+// VERSIUNE LICENTA
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
